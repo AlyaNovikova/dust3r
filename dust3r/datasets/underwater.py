8d0f98a4209aed64a7b5f845fb77e9a9a35e319e
AlyaNovikova <aleksnovikova@gmail.com>
2024-12-12 00:49:50 +0100

    finish with pairs

--- dust3r/datasets/underwater.py
+++ dust3r/datasets/underwater.py
@@ -9,9 +9,15 @@
 import os.path as osp
 import cv2
 import numpy as np
+import torch
 
+from dust3r.utils.geometry import depthmap_to_absolute_camera_coordinates
 from dust3r.datasets.base.base_stereo_view_dataset import BaseStereoViewDataset
 from dust3r.utils.image import imread_cv2
+from collections import defaultdict
+from sklearn.neighbors import NearestNeighbors
+from tqdm import tqdm
+from scipy.spatial import cKDTree
 
 
 class UnderWaterDataset(BaseStereoViewDataset):
@@ -39,6 +45,8 @@ class UnderWaterDataset(BaseStereoViewDataset):
             self.split_data = self._get_split_data(train=False)
         else:
             raise ValueError("split must be either 'train' or 'test'")
+
+        self.selected_pairs = self._create_pairs_indexes()
 
     def _get_split_data(self, train=True):
         """
@@ -102,7 +110,7 @@ class UnderWaterDataset(BaseStereoViewDataset):
         return image_poses
 
     def __len__(self):
-        return len(self.split_data) - 1
+        return len(self.selected_pairs)
 
     def _quaternion_to_rotation_matrix(self, q0, q1, q2, q3):
         r00 = 2 * (q0 * q0 + q1 * q1) - 1
@@ -140,11 +148,219 @@ class UnderWaterDataset(BaseStereoViewDataset):
         ])
         return K
 
+    def _get_one_view(self, idx, indexes):
+        if isinstance(idx, tuple):
+            # the idx is specifying the aspect-ratio
+            idx, ar_idx = idx
+        else:
+            assert len(self._resolutions) == 1
+            ar_idx = 0
+
+        # set-up the rng
+        if self.seed:  # reseed for each __getitem__
+            self._rng = np.random.default_rng(seed=self.seed + idx)
+        elif not hasattr(self, '_rng'):
+            seed = torch.initial_seed()  # this is different for each dataloader process
+            self._rng = np.random.default_rng(seed=seed)
+
+        # over-loaded code
+        resolution = self._resolutions[ar_idx]  # DO NOT CHANGE THIS (compatible with BatchedRandomSampler)
+
+        idx_current = idx
+
+        image_id = indexes[idx_current]
+        pose = self.image_poses.get(image_id)
+        if not pose:
+            raise ValueError(f"image id {image_id} not found in images.txt")
+
+        camera_id = pose["camera_id"]
+        intrinsics = self.intrinsic_camera.get(camera_id)
+        if not intrinsics:
+            raise ValueError(f"camera id {camera_id} not found in cameras.txt")
+
+        qw, qx, qy, qz = pose["rotation"]
+        tx, ty, tz = pose["translation"]
+        camera_pose = self._extrinsics_matrix(qw, qx, qy, qz, tx, ty, tz)
+
+        fx = intrinsics['fx']
+        fy = intrinsics['fy']
+        cx = intrinsics['cx']
+        cy = intrinsics['cy']
+
+        camera_intrinsics = self._intrinsic_matrix(fx, fy, cx, cy)
+
+        image_name = pose["image_name"]
+        rgb_image_path = osp.join(self.images_path, image_name)
+        depthmap_path = osp.join(self.depthmap_path, f"{image_name}.geometric.bin")
+
+        rgb_image = imread_cv2(rgb_image_path)
+        if rgb_image is None:
+            raise FileNotFoundError(f"RGB image {image_name} not found at {rgb_image_path}")
+
+        with open(depthmap_path, "rb") as fid:
+            width, height, channels = np.genfromtxt(
+                fid, delimiter="&", max_rows=1, usecols=(0, 1, 2), dtype=int
+            )
+            fid.seek(0)
+            num_delimiter = 0
+            byte = fid.read(1)
+            while True:
+                if byte == b"&":
+                    num_delimiter += 1
+                    if num_delimiter >= 3:
+                        break
+                byte = fid.read(1)
+            array = np.fromfile(fid, np.float32)
+        array = array.reshape((width, height, channels), order="F")
+        depthmap = np.transpose(array, (1, 0, 2)).squeeze()
+
+        if depthmap is None:
+            raise FileNotFoundError(f"Depth map for {image_name} not found at {depthmap_path}")
+
+        depthmap[~np.isfinite(depthmap)] = 0
+
+        rgb_image, depthmap, camera_intrinsics = self._crop_resize_if_necessary(
+            rgb_image, depthmap, camera_intrinsics, resolution, rng=self._rng, info=image_name)
+
+        return dict(
+            img=rgb_image,
+            depthmap=depthmap,
+            camera_pose=camera_pose.astype(np.float32),
+            camera_intrinsics=camera_intrinsics.astype(np.float32),
+            dataset='UnderWaterDataset',
+            label=self.ROOT,
+            instance=image_name
+        )
+
+    def compute_iou(self, pointcloud1, pointcloud2, iou_threshold=0.75, pixels_count=40000, distance_threshold=1):
+        if len(pointcloud1) > pixels_count:
+            pointcloud1 = pointcloud1[np.random.choice(len(pointcloud1), pixels_count, replace=False)]
+        if len(pointcloud2) > pixels_count:
+            pointcloud2 = pointcloud2[np.random.choice(len(pointcloud2), pixels_count, replace=False)]
+
+        nbrs = NearestNeighbors(n_neighbors=1, algorithm='kd_tree').fit(pointcloud2)
+        distances, indices = nbrs.kneighbors(pointcloud1)
+        intersection1 = np.count_nonzero(distances.flatten() < distance_threshold)
+
+        # nbrs = NearestNeighbors(n_neighbors=1, algorithm = 'kd_tree').fit(pointcloud1)
+        # distances, indices = nbrs.kneighbors(pointcloud2)
+        # intersection2 = np.count_nonzero(distances.flatten() < distance_threshold)
+
+        # iou = min(intersection1 / pixels_count, intersection2 / pixels_count)
+        iou = intersection1 / len(pointcloud1)
+
+        if iou > iou_threshold:
+            return 0.0
+
+        return intersection1
+
+    def simulate_occlusion(self, point_cloud, radius):
+        """
+        Simulates occlusion by attaching a ball of fixed radius to each 3D point.
+        Points occluded by another point within the radius are removed.
+        """
+        if len(point_cloud.shape) == 3:
+            point_cloud = point_cloud.reshape(-1, 3)
+        return point_cloud
+        # tree = cKDTree(point_cloud)
+        #
+        # indices = tree.query_ball_tree(tree, radius)
+        #
+        # visible_mask = np.full(len(point_cloud), True, dtype=bool)
+        # for i, neighbors in enumerate(indices):
+        #     if visible_mask[i]:
+        #         for neighbor in neighbors[1:]:
+        #             visible_mask[neighbor] = False
+        #
+        # visible_points = point_cloud[visible_mask]
+        # return visible_points
+
+    def compute_iou_with_occlusion(self, pc1, pc2, iou_threshold, radius=0.05):
+        """
+        Computes the IoU between two 3D point clouds with artificial occlusion.
+        """
+        visible_pc1 = self.simulate_occlusion(pc1, radius)
+        visible_pc2 = self.simulate_occlusion(pc2, radius)
+
+        return self.compute_iou(visible_pc1, visible_pc2, iou_threshold)
+
+    def quality_pair_score(self, iou, alpha):
+        """
+        Compute the quality pair score s = IoU × 4 cos(α)(1 - cos(α))
+        """
+        angle_term = 4 * np.cos(alpha) * (1 - np.cos(alpha))
+        return iou * angle_term if angle_term > 0 else 0
+
+    def select_best_pairs(self, pairs_indexes, iou_threshold=0.75, score_threshold=0.001):
+        """
+        Select the best image pairs using a greedy algorithm
+        """
+        pair_scores = []
+
+        for pair_ij in tqdm(pairs_indexes):
+            # if iter_cur % 100 == 0:
+            #     print(f'pairs selection: {iter_cur} out of {len(pairs_indexes)}')
+            i, j = pair_ij
+            img1 = self._get_one_view(i, self.split_data)
+            img2 = self._get_one_view(j, self.split_data)
+
+            pts3d1, _ = depthmap_to_absolute_camera_coordinates(**img1)
+            pts3d2, _ = depthmap_to_absolute_camera_coordinates(**img2)
+
+            # print('i and j', i, j)
+            iou = self.compute_iou_with_occlusion(pts3d1, pts3d2, iou_threshold)
+            if iou == 0:
+                continue
+
+            pose1, pose2 = img1['camera_pose'], img2['camera_pose']
+            rotation_diff = pose1[:3, :3].T @ pose2[:3, :3]  # Relative rotation matrix
+            alpha = np.arccos(np.clip((np.trace(rotation_diff) - 1) / 2, -1, 1))  # Angle in radians
+
+            score = self.quality_pair_score(iou, alpha)
+            if score > score_threshold:
+                pair_scores.append((score, i, j))
+
+        pair_scores.sort(key=lambda x: x[0], reverse=True)
+        return pair_scores
+
+    def _create_pairs_indexes(self, pairs_per_image=10, step=3, image_threshold=50):
+        n = len(self.split_data)
+
+        pairs = []
+
+        for i in range(n):
+            next_indices = list(range(i + 1, min(i + pairs_per_image + 1, n), step))
+            # next_indices = list(range(i + 1, min(i + 2, n)))
+            pairs.extend([[i, j] for j in next_indices])
+
+        pair_scores = self.select_best_pairs(pairs)
+        selected_pairs = []
+        used_images = defaultdict(int)
+
+        for score, i, j in pair_scores:
+            if used_images[i] > image_threshold or used_images[j] > image_threshold:
+                continue
+
+            selected_pairs.append((i, j))
+            used_images[i] += 1
+            used_images[j] += 1
+
+        print('LEN selected_pairs', len(selected_pairs))
+
+        for i in range(n - 1):
+            if (i, i + 1) not in selected_pairs:
+                selected_pairs.append((i, i + 1))
+
+        print('LEN selected_pairs with adjacent frames', len(selected_pairs))
+
+        return selected_pairs
+
+
     def _get_views(self, idx, resolution, rng):
         views = []
 
-        for idx_current in [idx, idx + 1]:
-            image_id = self.split_data[idx_current]
+        for idx_current in self.selected_pairs[idx]:
+            image_id = idx_current
             pose = self.image_poses.get(image_id)
             if not pose:
                 raise ValueError(f"image id {image_id} not found in images.txt")
